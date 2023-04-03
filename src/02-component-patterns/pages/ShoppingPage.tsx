4ee66ba6297afea77e6268edf73b24651e7948d5
crisbasualdo86 <crisbasualdo86@gmail.com>
2023-04-03 20:56:39 -0300

    Component Extend Styles listo

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -4,6 +4,7 @@ import {
     ProductImage,
     ProductTitle,
 } from '../components';
+import '../styles/custom-styles.css';
 
 export const ShoppingPage = () => {
     const product = {
@@ -24,16 +25,36 @@ export const ShoppingPage = () => {
                     flexWrap: 'wrap',
                 }}
             >
-                <ProductCard product={product}>
-                    <ProductCard.Image />
-                    <ProductCard.Title />
-                    <ProductCard.Buttons />
+                <ProductCard product={product} className="bg-dark text-white">
+                    <ProductCard.Image className="custom-img" />
+                    <ProductCard.Title className="text-bold" />
+                    <ProductCard.Buttons className="custom-buttons" />
                 </ProductCard>
 
-                <ProductCard product={product}>
-                    <ProductImage />
-                    <ProductTitle />
-                    <ProductButtons />
+                <ProductCard product={product} className="bg-dark text-white">
+                    <ProductImage
+                        className="custom-img"
+                        style={{
+                            boxShadow: '10px 10px 10px rgba(0, 0, 0, 0.2)',
+                        }}
+                    />
+                    <ProductTitle className="text-bold" />
+                    <ProductButtons className="custom-buttons" />
+                </ProductCard>
+
+                <ProductCard
+                    product={product}
+                    style={{ backgroundColor: '#78D1f8' }}
+                >
+                    <ProductImage
+                        style={{
+                            boxShadow: '10px 10px 10px rgba(0, 0, 0, 0.2)',
+                        }}
+                    />
+                    <ProductTitle style={{ fontWeight: 'bold' }} />
+                    <ProductButtons
+                        style={{ display: 'flex', justifyContent: 'end' }}
+                    />
                 </ProductCard>
             </div>
         </div>
