bd31d20d98707865e41269a75414faae3406409a
crisbasualdo86 <crisbasualdo86@gmail.com>
2023-04-14 18:33:54 -0300

    safasfa

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -5,7 +5,6 @@ import {
     ProductTitle,
 } from '../components';
 
-import '../styles/custom-styles.css';
 import { products } from '../data/products';
 
 const product = products[0];
@@ -18,7 +17,6 @@ export const ShoppingPage = () => {
 
             <ProductCard
                 product={product}
-                className="bg-dark text-white"
                 initialValues={{
                     count: 4,
                     maxCount: 10,
@@ -32,25 +30,9 @@ export const ShoppingPage = () => {
                     maxCount,
                 }) => (
                     <>
-                        <ProductImage
-                            className="custom-img"
-                            style={{
-                                boxShadow: '10px 10px 10px rgba(0, 0, 0, 0.2)',
-                            }}
-                        />
-                        <ProductTitle className="text-bold" />
-                        <ProductButtons className="custom-buttons" />
-                        <button onClick={reset}>Reset</button>
-
-                        <button onClick={() => increaseBy(-2)}>-2</button>
-
-                        {!isMaxCountReached && (
-                            <button onClick={() => increaseBy(2)}>+2</button>
-                        )}
-
-                        <span>
-                            {count} - {maxCount}
-                        </span>
+                        <ProductImage />
+                        <ProductTitle />
+                        <ProductButtons />
                     </>
                 )}
             </ProductCard>
